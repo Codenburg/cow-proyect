585e0f0ba11517f9c504c24811f982ea3359a817
Tomas Oldenburg <oldenburgtomas@hotmail.com>
2023-02-21 21:11:12 -0300

    starting project

---
+++ cowapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from cowapp import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
